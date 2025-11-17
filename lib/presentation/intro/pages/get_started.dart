da7c932b183b1e5a6ccc682998d604d905cb62b9
hakimxiao <alhakimabusolihin@gmail.com>
2025-11-17 10:10:54 +0700

    Create get_startedPage :  - Membuat halaman get started yang akan di tampilkan seteleah spl;ash screen.
    Create common widget dynamic BasicButton Component :
     - Membuat class widget yang didalamnya adalah button yang sudah di styling dan dinamis
    
     Repairing & Optimizing :
      - Sedikit melakukkan perubahan karena masalah warna pada button dan menambahkan properti elevation pda core theme app.

--- lib/presentation/intro/pages/get_started.dart
+++ lib/presentation/intro/pages/get_started.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:spotify_app/common/widgets/button/basic_app_button.dart';
+import 'package:spotify_app/core/configs/assets/app_images.dart';
+import 'package:spotify_app/core/configs/assets/app_vectors.dart';
+import 'package:spotify_app/core/configs/theme/app_colors.dart';
 
 class GetStarted extends StatelessWidget {
   const GetStarted({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(AppImages.introBG),
+              ),
+            ),
+          ),
+          Container(color: Colors.black.withValues(alpha: 0.15)),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 40, horizontal: 40),
+            child: Column(
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: SvgPicture.asset(AppVectors.logo),
+                ),
+                const Spacer(),
+                const Text(
+                  'Nikmati Pengalaman Musik Terbaik',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+                const SizedBox(height: 21),
+                const Text(
+                  'Temukan musik baru yang sesuai dengan mood kamu.\nMusik terbaik untuk menemani setiap aktivitasmu.\nHiburan musik tanpa iklan dan tanpa gangguan.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: AppColors.grey,
+                    fontSize: 13,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                BasicAppButton(onPressed: () {}, title: 'Get Started'),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
