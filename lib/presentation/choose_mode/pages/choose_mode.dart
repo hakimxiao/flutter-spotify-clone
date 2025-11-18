3889c5996940355431cc42d3120d66ac7df89472
hakimxiao <alhakimabusolihin@gmail.com>
2025-11-18 09:18:15 +0700

    Implemen state management  for choose_mode page:
     - Menggunakan bloc dan hydrated_bloc untuk mengatur state management dimana kita merubah theme dengan cara memeasukkan value baru ke emit.
     - Di main kita melakukkan inisialisasi HydratedStorage karena state theme itu akan disimpan ke app dan yang bisa menghandle ini jika kita menggunakan bloc adalah hydrated_bloc storage.
     - sesudah inisialisasi dan cubit bloc sudah di buat tugas selanjutnya adalah melakukkan injeksi di main agar semua pages mengadopsi theme yang baru. jadi pada scaffold kita bungkus  dia dengan MultiBlocProvider deklarasikan providers terkait, pada child MultiBlocProvider kita isikan dengan BlocBuilder dimana dia bertipe cubit yang kita buat tadi dengan object value ThemeMode || BlocBuilder<ThemeCubit, ThemeMode> ---- barulah di bagian builder kita mereturn material app yang dimaana themeMode nya sekarang di ambil dari param builder yaitu mode builder: (context, mode) =>

--- lib/presentation/choose_mode/pages/choose_mode.dart
+++ lib/presentation/choose_mode/pages/choose_mode.dart
@@ -1,11 +1,13 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:spotify_app/common/widgets/button/basic_app_button.dart';
 import 'package:spotify_app/core/configs/assets/app_images.dart';
 import 'package:spotify_app/core/configs/assets/app_vectors.dart';
 import 'package:spotify_app/core/configs/theme/app_colors.dart';
+import 'package:spotify_app/presentation/choose_mode/bloc/theme_cubit.dart';
 
 class ChooseMode extends StatelessWidget {
   const ChooseMode({super.key});
@@ -17,7 +19,7 @@ class ChooseMode extends StatelessWidget {
         children: [
           Container(
             padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 40),
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               image: DecorationImage(
                 fit: BoxFit.fill,
                 image: AssetImage(AppImages.chooseModeBG),
@@ -48,24 +50,33 @@ class ChooseMode extends StatelessWidget {
                   children: [
                     Column(
                       children: [
-                        ClipOval(
-                          child: BackdropFilter(
-                            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                            child: Container(
-                              height: 80,
-                              width: 80,
-                              decoration: BoxDecoration(
-                                color: Color(0xff30393C).withValues(alpha: 0.5),
-                                shape: BoxShape.circle,
-                              ),
-                              child: SvgPicture.asset(
-                                AppVectors.sun,
-                                fit: BoxFit.none,
+                        GestureDetector(
+                          onTap: () {
+                            context.read<ThemeCubit>().updateTheme(
+                              ThemeMode.light,
+                            );
+                          },
+                          child: ClipOval(
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                              child: Container(
+                                height: 80,
+                                width: 80,
+                                decoration: BoxDecoration(
+                                  color: Color(
+                                    0xff30393C,
+                                  ).withValues(alpha: 0.5),
+                                  shape: BoxShape.circle,
+                                ),
+                                child: SvgPicture.asset(
+                                  AppVectors.sun,
+                                  fit: BoxFit.none,
+                                ),
                               ),
                             ),
                           ),
                         ),
-                        SizedBox(height: 15),
+                        const SizedBox(height: 15),
                         Text(
                           'Light Mode',
                           style: TextStyle(
@@ -76,22 +87,31 @@ class ChooseMode extends StatelessWidget {
                         ),
                       ],
                     ),
-                    SizedBox(width: 40),
+                    const SizedBox(width: 40),
                     Column(
                       children: [
-                        ClipOval(
-                          child: BackdropFilter(
-                            filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                            child: Container(
-                              height: 80,
-                              width: 80,
-                              decoration: BoxDecoration(
-                                color: Color(0xff30393C).withValues(alpha: 0.5),
-                                shape: BoxShape.circle,
-                              ),
-                              child: SvgPicture.asset(
-                                AppVectors.moon,
-                                fit: BoxFit.none,
+                        GestureDetector(
+                          onTap: () {
+                            context.read<ThemeCubit>().updateTheme(
+                              ThemeMode.dark,
+                            );
+                          },
+                          child: ClipOval(
+                            child: BackdropFilter(
+                              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                              child: Container(
+                                height: 80,
+                                width: 80,
+                                decoration: BoxDecoration(
+                                  color: Color(
+                                    0xff30393C,
+                                  ).withValues(alpha: 0.5),
+                                  shape: BoxShape.circle,
+                                ),
+                                child: SvgPicture.asset(
+                                  AppVectors.moon,
+                                  fit: BoxFit.none,
+                                ),
                               ),
                             ),
                           ),
